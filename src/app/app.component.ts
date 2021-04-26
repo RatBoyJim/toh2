c66bb562583d9fca0828296365b8caf48d5be770
RatBoyJim <michaelanderson@michaels-mbp.home>
2021-04-26 13:14:24 +0100

    main app working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angulartoh2';
+  title = 'Tour Of Heroes 2 - Tour Harder';
 }
